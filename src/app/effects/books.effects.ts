d0808a7f9f844bdc72a0aecaac14e13359787d96
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-06-26 08:17:32 +0200

    [add] books effects

---
+++ src/app/effects/books.effects.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Effect, Actions, toPayload } from '@ngrx/effects';
+import { Action } from '@ngrx/store';
+import { Observable } from 'rxjs/Observable';
+import { of } from 'rxjs/observable/of';
+
+import { GoogleBooksService } from '../services/google-books.service';
+import * as books from '../actions/books.actions';
+
+@Injectable()
+export class BooksEffects {
+    @Effect()
+    onSearch$: Observable<Action> = this.actions$
+        .ofType(books.SEARCH)
+        .map(toPayload)
+        .switchMap(query => this.gbService.search(query)
+            .map(bks => new books.SearchSuccessAction(bks))
+            .catch(err => of(new books.SearchFailAction(err))));
+
+    constructor(private actions$: Actions, private gbService: GoogleBooksService) { }
+}

9c08e6fd53f347c223150e3e0a2ec3711e8ac808
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-06-27 12:26:13 +0200

    [add] add to cart component

---
+++ src/app/components/add-to-cart/add-to-cart.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-add-to-cart',
+  templateUrl: './add-to-cart.component.html',
+  styleUrls: ['./add-to-cart.component.css']
+})
+export class AddToCartComponent {
+  @Input('isInCart')
+  isInCart: boolean;
+}

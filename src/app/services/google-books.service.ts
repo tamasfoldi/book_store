b859fdfea0df17db45284be7e82e67782b022a81
Tamas Janos Foldi <fo.tamas@gmail.com>
2017-06-28 08:15:41 +0200

    [add] cache to getBook

--- src/app/services/google-books.service.ts
+++ src/app/services/google-books.service.ts
@@ -1,13 +1,19 @@
 import { Injectable, Inject } from '@angular/core';
 import { Http } from '@angular/http';
-import { BOOKS_API_BASE } from './tokens';
+import { Store } from '@ngrx/store';
+import { BOOKS_API_BASE, SCHEMA_NAME, CART_DB_NAME } from './tokens';
 import { Observable } from 'rxjs/Rx';
 import { Book } from '../models/book';
+import { State, getBooks } from '../reducers';
+
 
 @Injectable()
 export class GoogleBooksService {
 
-  constructor( @Inject(BOOKS_API_BASE) private baseUrl: string, private http: Http) { }
+  constructor(
+    @Inject(BOOKS_API_BASE) private baseUrl: string,
+    private http: Http,
+    private store: Store<State>) { }
 
   search(query: string): Observable<Book[]> {
     return this.http.get(`${this.baseUrl}?q=${query}`)
@@ -15,8 +21,10 @@ export class GoogleBooksService {
   }
 
   getBook(bookId: string): Observable<Book> {
-    return this.http.get(`${this.baseUrl}/${bookId}`)
-      .map(rsp => rsp.json());
+    return this.store.select(getBooks)
+      .map(books => books[bookId])
+      .switchMap(book => book ? Observable.of(book) : this.http.get(`${this.baseUrl}/${bookId}`)
+        .map(rsp => rsp.json()));
   }
 
 }
